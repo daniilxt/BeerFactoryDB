8b96f65aad7f69b84995916bbfb60306a9f1e329
daniilxt <daniil-firsov@mail.ru>
2020-12-13 03:21:25 +0300

    added scelet of client list

---
+++ src/main/java/pojo/OrderPosition.kt
@@ -0,0 +1,8 @@
+package pojo
+
+data class OrderPosition(
+    var idOrderPosition: Long = 0,
+    var number: Long = 0,
+    var idOrder: Long = 0,
+    var idBeerKind: Long = 0
+)
--- src/main/java/pojo/Orders.kt
+++ src/main/java/pojo/Orders.kt
@@ -2,10 +2,9 @@ package pojo
 
 import java.sql.Date
 
-class Orders {
-    var idOrder: Long = 0
-    var idClient: Long = 0
-    var idManager: Long = 0
-    var date: Date? = null
+data class Orders(
+    var idOrder: Long = 0,
+    var manager: Long = 0,
+    var date: Date? = null,
     var status: String? = null
-}
+)
